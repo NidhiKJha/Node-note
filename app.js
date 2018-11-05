6c85a57e36dd2b3a16116c7686d2f8ff1e634abf
Nidhi Kumari <nidhi1691996@outlook.com>
2018-11-05 19:08:55 +0530

    to get user printed with cancat

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const fs = require('fs');
 const os = require('os');
 var user = os.userInfo();
 
-console.log(user);
 
 
 
 
-//fs.appendFileSync('greetings.txt','HelloWorld');
+
+fs.appendFileSync('greetings.txt','Hello' + user.username);
