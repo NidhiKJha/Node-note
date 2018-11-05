145012961b9bd58e50bf6d131c8cfd945dfd0a3c
Nidhi Kumari <nidhi1691996@outlook.com>
2018-11-05 19:02:59 +0530

    appendosmodule

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 console.log("Starting app.");
-const fs =require('fs');
-const os =require('os');
+const fs = require('fs');
 
+const os = require('os');
 var user = os.userInfo();
-console.log('user');
 
-//fs.appendFile('greetings.txt','HelloWorld');
+console.log(user);
+
+
+
+
+//fs.appendFileSync('greetings.txt','HelloWorld');
