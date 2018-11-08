68094275b8a658415010630d211a8be2d0a81966
Nidhi Kumari <nidhi1691996@outlook.com>
2018-11-09 00:35:21 +0530

    op

--- app.js
+++ app.js
@@ -2,16 +2,8 @@
 console.log("Starting app.js");
 const fs = require('fs');
 
-const os = require('os');
+
 const _=require('lodash');
 
-//var user = os.userInfo();
+
 const notes = require('./notes.js');
-var filteredArray = _.uniq(["Mike"]);
-console.log(filteredArray);
-//console.log(_.isString(true));
-//console.log(_.isString('Andrew'));
-
-//var res = notes.addNote();
-
-//console.log('Result:', notes.add(9, -2));
