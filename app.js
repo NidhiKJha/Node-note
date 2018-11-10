fd2e831bcbad87e85e9b7b4e2022eca1682a061c
Nidhi Kumari <nidhi1691996@outlook.com>
2018-11-10 23:39:01 +0530

    corrections

--- app.js
+++ app.js
@@ -8,16 +8,16 @@ const _=require('lodash');
 
 const notes = require('./notes.js');
 var command = process.argv[2];
-console.log('Command:' , Command) ;
+console.log('Command:' , command) ;
 
 if (command === 'add'){
-  console.log('Adding new // NOTE: ');
+  console.log('Adding new note ');
 
 
 }else if (command === 'list'){
   console.log('Listing all notes');
 } else{
-  console.log("Command not recogonised")
+  console.log("Command not recogonised");
 }
 
 /*var command = process.argv[2];
