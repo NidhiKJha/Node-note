29d2f4a2ba1200aea8362a1f5f3d155a43e5cb5c
Nidhi Kumari <nidhi1691996@outlook.com>
2018-11-11 22:50:51 +0530

    yes

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const fs = require('fs');
 
 
 const _=require('lodash');
+const yargs=require('yargs');
 
 
 const notes = require('./notes.js');
+
+const argv = yargs.argv;
 var command = process.argv[2];
 console.log('Command:', command) ;
 console.log(process.argv);
